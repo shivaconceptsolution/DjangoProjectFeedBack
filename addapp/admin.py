53c8793efecb8da67e25a748c5421ecd0e83b76f
shiva <50239824+shivaconceptsolution@users.noreply.github.com>
2020-01-31 20:42:52 -0800

    Add files via upload

---
+++ addapp/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import Register,Feedback
+admin.site.register(Register)
+admin.site.register(Feedback)
---
+++ addapp/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Register(models.Model):
+  emailid= models.CharField(max_length=200)
+  password= models.CharField(max_length=200)
+  mobile= models.CharField(max_length=200)
+  fullname= models.CharField(max_length=200)
+  def __str__(self):
+  	return "emailid is "+self.emailid +" password is "+self.password +" mobile is "+self.mobile+" fullname is "+self.fullname
+class Feedback(models.Model):
+  feedby= models.CharField(max_length=200)
+  feedto= models.CharField(max_length=200)
+  feeddesc= models.CharField(max_length=200)
+  feedbackrate= models.IntegerField()
+  def __str__(self):
+  	return "feedby  "+self.feedby+" feed to "+self.feedto +" desc "+self.feeddesc+" Rating is "+str(self.feedbackrate)
